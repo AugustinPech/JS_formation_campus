6d1a540229b13b82ef86286b2b90368ddce4e1e2
AugustinPech <80385451+AugustinPech@users.noreply.github.com>
2024-03-29 17:22:00 +0100

    first commit

--- json/data/data.js
+++ json/data/data.js
@@ -1,7 +1,7 @@
 let data = [
     {
         title: "Counting people app",
-        desc: "A really simple app for counting people.",
+        desc: "A really simple app for lots counting people somewhere.",
         imgPath: "/JS/img/CountingPeople.jpeg",
         appPath: "/JS/pages/games/Scimba.html"
     },
