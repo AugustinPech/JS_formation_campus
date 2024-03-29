c0f8fd83dab894644849fd4ff455b0640c746299
AugustinPech <80385451+AugustinPech@users.noreply.github.com>
2024-03-29 17:25:27 +0100

    fixed links on branch dev

--- json/data/data.js
+++ json/data/data.js
@@ -2,7 +2,7 @@ let data = [
     {
         title: "Counting people app",
         desc: "A really simple app for lots counting people somewhere.",
-        imgPath: "/JS/img/CountingPeople.jpeg",
-        appPath: "/JS/pages/games/Scimba.html"
+        imgPath: "/img/CountingPeople.jpeg",
+        appPath: "/pages/games/Scimba.html"
     },
 ]
