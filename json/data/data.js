18f937ee175ba3369314e12504b9b33f28a8e70d
AugustinPech <80385451+AugustinPech@users.noreply.github.com>
2024-03-29 17:50:37 +0100

    trying to get de GitHub page to work for christ sake

--- json/data/data.js
+++ json/data/data.js
@@ -2,7 +2,7 @@ let data = [
     {
         title: "Counting people app",
         desc: "A really simple app for counting lots people somewhere.",
-        imgPath: "/img/CountingPeople.jpeg",
-        appPath: "/pages/games/Scimba.html"
+        imgPath: "img/CountingPeople.jpeg",
+        appPath: "pages/games/Scimba.html"
     },
 ]
--- json/data/testDatas.js
+++ json/data/testDatas.js
@@ -2,12 +2,12 @@ let testDatas = [
     {
         title: "cool app 1",
         desc: "The findIndex() method of Array instances returns the index of the first element in an array that satisfies the provided testing function. If no elements satisfy the testing function, -1 is returned. See also the find() method, which returns the first element that satisfies the testing function (rather than its index).",
-        imgPath: "/img/testimg.png",
-        appPath: "/pages/games/game1.html"
+        imgPath: "img/testimg.png",
+        appPath: "pages/games/game1.html"
     },    {
         title: "cool app 1",
         desc: "The findIndex() method of Array instances returns the index of the first element in an array that satisfies the provided testing function. If no elements satisfy the testing function, -1 is returned. See also the find() method, which returns the first element that satisfies the testing function (rather than its index).",
-        imgPath: "/img/testimg.png",
-        appPath: "/pages/games/game1.html"
+        imgPath: "img/testimg.png",
+        appPath: "pages/games/game1.html"
     }
 ]
