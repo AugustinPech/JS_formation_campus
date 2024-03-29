244a2b9f9e8c3e3d3db700a54403c1185f7c25e4
AugustinPech <80385451+AugustinPech@users.noreply.github.com>
2024-03-29 18:03:59 +0100

    trying to get de GitHub page to work for christ sake

--- json/data/data.js
+++ json/data/data.js
@@ -3,6 +3,6 @@ let data = [
         title: "Counting people app",
         desc: "A really simple app for counting lots people somewhere.",
         imgPath: "https://www.linksprite.com/wp-content/uploads/2019/03/counting-1024x722.jpg",
-        appPath: "pages/games/Scimba.html"
+        appPath: "https://augustinpech.github.io/JS_formation_campus/pages/games/Scimba.html"
     },
 ]
