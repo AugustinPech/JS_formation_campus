529ca9809312e051efe2c0f7159732ba8051ee27
AugustinPech <80385451+AugustinPech@users.noreply.github.com>
2024-03-29 18:02:51 +0100

    trying to get de GitHub page to work for christ sake

--- json/data/data.js
+++ json/data/data.js
@@ -2,7 +2,7 @@ let data = [
     {
         title: "Counting people app",
         desc: "A really simple app for counting lots people somewhere.",
-        imgPath: "img/CountingPeople.jpeg",
+        imgPath: "https://www.linksprite.com/wp-content/uploads/2019/03/counting-1024x722.jpg",
         appPath: "pages/games/Scimba.html"
     },
 ]
--- json/data/testDatas.js
+++ json/data/testDatas.js
@@ -2,12 +2,12 @@ let testDatas = [
     {
         title: "cool app 1",
         desc: "The findIndex() method of Array instances returns the index of the first element in an array that satisfies the provided testing function. If no elements satisfy the testing function, -1 is returned. See also the find() method, which returns the first element that satisfies the testing function (rather than its index).",
-        imgPath: "img/testimg.png",
+        imgPath: "https://wikiwandv2-19431.kxcdn.com/_next/image?url=https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/PNG_transparency_demonstration_1.png/640px-PNG_transparency_demonstration_1.png&w=640&q=50",
         appPath: "pages/games/game1.html"
     },    {
         title: "cool app 1",
         desc: "The findIndex() method of Array instances returns the index of the first element in an array that satisfies the provided testing function. If no elements satisfy the testing function, -1 is returned. See also the find() method, which returns the first element that satisfies the testing function (rather than its index).",
-        imgPath: "img/testimg.png",
+        imgPath: "https://wikiwandv2-19431.kxcdn.com/_next/image?url=https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/PNG_transparency_demonstration_1.png/640px-PNG_transparency_demonstration_1.png&w=640&q=50",
         appPath: "pages/games/game1.html"
     }
 ]
