e9ffbea929f2fa1d95da84567900373954bb2c9c
AugustinPech <80385451+AugustinPech@users.noreply.github.com>
2024-04-02 16:53:51 +0200

    api works it is like magic

--- json/data/data.js
+++ json/data/data.js
@@ -1,13 +1,47 @@
 let data = [
     {
         title: "Counting people app",
-        desc: "A really simple app for counting lots people somewhere.",
-        imgPath: "https://www.linksprite.com/wp-content/uploads/2019/03/counting-1024x722.jpg",
+        description: "A really simple app for counting lots people somewhere.",
+        image: "https://www.linksprite.com/wp-content/uploads/2019/03/counting-1024x722.jpg",
         appPath: "https://augustinpech.github.io/JS_formation_campus/pages/games/counter.html"
     },    {
         title: "Simple BlackJack app",
-        desc: " A cool thing.",
-        imgPath: "https://live.staticflickr.com/3656/3496497359_610032ef17_c.jpg",
+        description: " A cool thing.",
+        image: "https://live.staticflickr.com/3656/3496497359_610032ef17_c.jpg",
         appPath: "../../pages/games/blackJack.html"
     },
 ]
+
+class Pokemon {
+    constructor(i) {
+        // let i = Math.floor(Math.random()*152+1)
+        this.appPath="https://pokeapi.co/api/v2/pokemon/"+i+"/"
+        fetch(this.appPath)
+            .then(response => response.json())
+            .then(a => { 
+                this.title = a.name
+                this.image = a.sprites.front_default
+                // this.description = "Quis enim nulla enim nulla ea elit in. Aliqua velit reprehenderit anim amet in aliquip non officia eu duis irure et sunt labore. In magna laboris do sint. Laboris ullamco est ex aliqua sint."
+                fetch(a.species.url).then(response => response.json())
+                    .then(b=> {
+                        let entries=[]
+                        b.flavor_text_entries.forEach(item => { if(item.language.name === "en"){entries.push(item)}})
+                        let maxIndex = entries.length
+                        let index= Math.floor(Math.random()*maxIndex)
+                        let element = entries[index].flavor_text        
+                        this.description=element
+                        let parentBlock=document.getElementById("catalogContainer")
+                        parentBlock.className= " d-flex-row"
+                        let newBlock = parentBlock.appendChild(document.createElement("div"))
+                        cardElement(this, newBlock)
+                    })
+            }).catch(error => console.error("Error : ", error, i))
+        
+    }
+}
+
+function pokemons(number){
+    for (let i=1; i<number; i++){
+        new Pokemon(i)
+    }
+}
--- json/games/blackJack.js
+++ json/games/blackJack.js
@@ -1,5 +1,5 @@
 let deck1 = [
-    { name: 'hearts_ace', value: 1 },
+    { name: 'hearts_ace', value: 11 },
     { name: 'hearts_2', value: 2 },
     { name: 'hearts_3', value: 3 },
     { name: 'hearts_4', value: 4 },
@@ -13,7 +13,7 @@ let deck1 = [
     { name: 'hearts_queen', value: 10 },
     { name: 'hearts_king', value: 10 },
 
-    { name: 'diamonds_ace', value: 1 },
+    { name: 'diamonds_ace', value: 11 },
     { name: 'diamonds_2', value: 2 },
     { name: 'diamonds_3', value: 3 },
     { name: 'diamonds_4', value: 4 },
@@ -27,7 +27,7 @@ let deck1 = [
     { name: 'diamonds_queen', value: 10 },
     { name: 'diamonds_king', value: 10 },
 
-    { name: 'clubs_ace', value: 1 },
+    { name: 'clubs_ace', value: 11 },
     { name: 'clubs_2', value: 2 },
     { name: 'clubs_3', value: 3 },
     { name: 'clubs_4', value: 4 },
@@ -41,7 +41,7 @@ let deck1 = [
     { name: 'clubs_queen', value: 10 },
     { name: 'clubs_king', value: 10 },
 
-    { name: 'spades_ace', value: 1 },
+    { name: 'spades_ace', value: 11 },
     { name: 'spades_2', value: 2 },
     { name: 'spades_3', value: 3 },
     { name: 'spades_4', value: 4 },
@@ -59,6 +59,7 @@ let deck1 = [
 let addPlayerBTN= document.querySelector('#addPlayer')
 let startGameBTN= document.querySelector('#startGame')
 let nextRoundBTN= document.querySelector('#nextRound')
+let newGameBTN= document.querySelector('#newGame')
 let board = document.querySelector('#board')
 function resetBlock(block) {
     while (block.firstChild!=null){
@@ -75,6 +76,10 @@ function prependNewChildElement(parent, childType, idName){
     child.setAttribute('id',idName)
     parent.prepend(child)
 }
+function killChild(parent, id){
+    let child= document.getElementById(id)
+    parent.removeChild(child)
+}
 class Card {
     constructor(){
         if (deck1.length>0){
@@ -82,7 +87,7 @@ class Card {
             let newCard = deck1.splice(index, 1)[0]
             this.name = newCard.name
             this.value = newCard.value
-            this.imgPath = "https://tekeye.uk/playing_cards/images/svg_playing_cards/fronts/"+newCard.name+".svg"
+            this.image = "https://tekeye.uk/playing_cards/images/svg_playing_cards/fronts/"+newCard.name+".svg"
             console.log("deck : "+deck1.length);
         } else {
             this.resetDeck()
@@ -164,43 +169,60 @@ class Player {
         playerZone.lastChild.innerText= "Stop"
         playerZone.lastChild.className= "stopsDrawing"
         playerZone.lastChild.addEventListener("click", () => this.isDrawing=false)
+        if (this.name!="dealer"){
+            newChildElement(playerZone,"button",this.name+"LeavesGame")
+            playerZone.lastChild.setAttribute('id',this.name+"LeavesGame")
+            playerZone.lastChild.innerText= "Leave the game"
+            playerZone.lastChild.className= "playerLeavesGame"
+            playerZone.lastChild.addEventListener("click", () => this.leavesGame())
+            playerZone.lastChild.style.display="none"
+        }
     }
     showCardsInHand(){
         let playerZone = document.querySelector('#'+this.name+'Zone')
-            let handDiv = document.createElement("div")
-            playerZone.prepend(handDiv)
-            handDiv.className =  "playerHand"
-            handDiv.setAttribute('id',this.name+"Hand")
-            this.hand.forEach(card => {
-                let cardImg = document.createElement("img")
-                cardImg.className="cardImg"
-                cardImg.src=card.imgPath
-                handDiv.appendChild(cardImg)})
+        let handDiv = document.createElement("div")
+        playerZone.prepend(handDiv)
+        handDiv.className =  "playerHand"
+        handDiv.setAttribute('id',this.name+"Hand")
+        this.hand.forEach(card => {
+            let cardImg = document.createElement("img")
+            cardImg.className="cardImg"
+            cardImg.src=card.image
+            handDiv.appendChild(cardImg)})
+    }
+    updateShowCardsInHand(){
+        let handDiv = document.querySelector("#"+this.name+"Hand")
+        resetBlock(handDiv)
+        this.hand.forEach(card => {
+            let cardImg = document.createElement("img")
+            cardImg.className="cardImg"
+            cardImg.src=card.image
+            handDiv.appendChild(cardImg)})
+    }
+    updateSum(){
+        this.sum=0
+        this.hand.forEach(card => {
+            this.sum+=card.value
+        })
+        let playerZone=document.querySelector("#"+this.name+"TopBar")
+        playerZone.textContent= this.name + " " + this.sum
+        this.didIWin()
+    }
+    didIWin(){
+        if (this.sum===21 && this.hand.length==2){
+            this.isDrawing=false
+            console.log(this.name + " BalckJack yeepee yo")
+        } else if (this.sum>21){
+            this.isDrawing=false
+            console.log(this.name + " Lost the game")
         }
-        updateShowCardsInHand(){
-            let handDiv = document.querySelector("#"+this.name+"Hand")
-            resetBlock(handDiv)
-            this.hand.forEach(card => {
-                let cardImg = document.createElement("img")
-                cardImg.className="cardImg"
-                cardImg.src=card.imgPath
-                handDiv.appendChild(cardImg)})
-        }
-        updateSum(){
-            this.sum=0
-            this.hand.forEach(card => {
-                this.sum+=card.value
-            })
-            let playerZone=document.querySelector("#"+this.name+"TopBar")
-            playerZone.textContent= this.name + " " + this.sum
-            if (this.sum===21){
-                this.isDrawing=false
-                console.log(this.name + " BalckJack yeepee yo")
-            } else if (this.sum>21){
-                this.isDrawing=false
-                console.log(this.name + " Lost the game")
-            }
-        }
+    }
+    leavesGame() {
+        let index= dealer.players.findIndex(element => element.name==this.name)
+        killChild(board, this.name+"Zone")
+        dealer.players.splice(index,1)
+    }
+
     }
 
 
@@ -221,16 +243,40 @@ class Dealer extends Player {
         this.players.forEach(
             (player)=> 
             {
-            player.beOnBoard()
-            this.giveNewCard(player)
-            this.giveNewCard(player)
-            player.showCardsInHand()
-            player.isDrawing=true
-            }
-        )
+                player.isDrawing=true
+                let playerZone=document.querySelector("#"+player.name+"Zone")
+                if (playerZone != null){
+                    this.giveNewCard(player)
+                    this.giveNewCard(player)
+                    player.updateShowCardsInHand()
+                    let leaveBTN=document.querySelector("#"+player.name+"LeavesGame")
+                    if (leaveBTN!=null) {leaveBTN.style.display="none"}
+                } else {
+                    player.beOnBoard()
+                    this.giveNewCard(player)
+                    this.giveNewCard(player)
+                    player.showCardsInHand()
+
+                }
+            })
         this.gameRunning=true
         startGameBTN.style.display="none"
         addPlayerBTN.style.display="none"
+        newGameBTN.style.display="block"
+        nextRoundBTN.style.display="block"
+    }
+    newGame() {
+        this.players.forEach(player => {
+            player.hand=[];
+            player.updateShowCardsInHand();
+            let leaveBTN=document.querySelector("#"+player.name+"LeavesGame")
+            if (leaveBTN!=null) {leaveBTN.style.display="block"}
+        })
+        startGameBTN.style.display="block"
+        addPlayerBTN.style.display="block"
+        newGameBTN.style.display="none"
+        nextRoundBTN.style.display="none"
+        
     }
     addPlayer(){
         let player = new Human
@@ -258,3 +304,4 @@ console.log(dealer)
 if (addPlayerBTN!=null){addPlayerBTN.addEventListener("click", dealer.addPlayer.bind(dealer))}
 if (startGameBTN!=null){startGameBTN.addEventListener("click", () => dealer.startGame())}
 if (nextRoundBTN!=null){nextRoundBTN.addEventListener("click", () => dealer.deal())}
+if (newGameBTN!=null){newGameBTN.addEventListener("click", () => dealer.newGame())}
