dee847a93b92143b5b483454fc5d4042cac3abec
AugustinPech <80385451+AugustinPech@users.noreply.github.com>
2024-04-03 09:13:45 +0200

    fixing links in the game cards

--- json/data/data.js
+++ json/data/data.js
@@ -16,6 +16,7 @@ class Pokemon {
     constructor(i) {
         // let i = Math.floor(Math.random()*152+1)
         this.appPath="https://pokeapi.co/api/v2/pokemon/"+i+"/"
+        this.id = i
         fetch(this.appPath)
             .then(response => response.json())
             .then(a => { 
@@ -35,8 +36,7 @@ class Pokemon {
                         let newBlock = parentBlock.appendChild(document.createElement("div"))
                         cardElement(this, newBlock)
                     })
-            }).catch(error => console.error("Error : ", error, i))
-        
+            }).catch(error => console.error("Error : ", error,"unable to load the pokemon number : ", i))
     }
 }
 
