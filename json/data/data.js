76b958333983a2daefdecf2938cbb90f4f1ae488
AugustinPech <80385451+AugustinPech@users.noreply.github.com>
2024-04-03 09:13:28 +0200

    fixing links in the game cards

--- json/data/data.js
+++ json/data/data.js
@@ -3,12 +3,12 @@ let data = [
         title: "Counting people app",
         description: "A really simple app for counting lots people somewhere.",
         image: "https://www.linksprite.com/wp-content/uploads/2019/03/counting-1024x722.jpg",
-        appPath: "https://augustinpech.github.io/JS_formation_campus/pages/games/counter.html"
+        appPath: "/JS_formation_campus/pages/games/counter.html"
     },    {
         title: "Simple BlackJack app",
         description: " A cool thing.",
         image: "https://live.staticflickr.com/3656/3496497359_610032ef17_c.jpg",
-        appPath: "/JS_formation_campus//pages/games/blackJack.html"
+        appPath: "/JS_formation_campus/pages/games/blackJack.html"
     },
 ]
 
